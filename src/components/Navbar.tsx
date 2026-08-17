0526bfac0edd2bbebef29828b6a949d094e6e6bf
Nikola Despenic <despenicnikola@gmail.com>
2026-08-17 18:09:50 +0200

    Pocetni izgled sajta

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <NavLink to="/" className="navbar__logo">
+        CATAN ONLINE
+      </NavLink>
+      <div className="navbar__links">
+        <NavLink to="/" end className="navbar__link">POČETNA</NavLink>
+        <NavLink to="/partije" className="navbar__link">PARTIJE</NavLink>
+        <NavLink to="/statistika" className="navbar__link">STATISTIKA</NavLink>
+        <NavLink to="/profil" className="navbar__link">PROFIL</NavLink>
+        <NavLink to="/login" className="navbar__link">ODJAVA</NavLink>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
